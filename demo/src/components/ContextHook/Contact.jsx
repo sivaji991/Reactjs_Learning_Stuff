7eef8c4f6d1d26c3dbc68346a241867e13d41945
Sivaji Avulamanda <sivajisalaar@gmail.com>
2026-01-09 20:41:50 +0530

    useContext completed

--- demo/src/components/ContextHook/Contact.jsx
+++ demo/src/components/ContextHook/Contact.jsx
@@ -4,6 +4,10 @@ import AppContext from './AppContext'
 const Contact = () => {
 
   const {phone, name} = useContext(AppContext)
+  // Most comon aim for Context api is to share current theme of your application with all its components or
+  
+  //to share the authenticated user with all of our components in our application or to sharethe result of an api call with all of our components in our application that will reduce the number of api calls
+
   return (
     <>
       <h2>Contact</h2>
