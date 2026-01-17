1429c2c4e2bc541b936869df3dec65d919b4fd2b
Sivaji Avulamanda <sivajisalaar@gmail.com>
2026-01-18 04:52:04 +0530

    new Chatbot project is completed with proper React Setup

---
+++ chatbot-project/src/components/ChatMessages.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from "react";
+import  ChatMessage from "./ChatMessage";
+
+import './ChatMessages.css';
+
+function ChatMessages( { chatMessages } ){
+
+        const chatMessagesRef = useRef(null);
+
+          useEffect(()=>{
+          const containerElem = chatMessagesRef.current
+          if(containerElem){
+            containerElem.scrollTop = containerElem.scrollHeight
+          }
+        }, [chatMessages]);
+        /*React will run this function -
+            -after component is created
+            -every time th component is updated
+            */
+       
+      return(
+        <div className = 'chat-messages-container'
+            ref = {chatMessagesRef}>
+          {chatMessages.map((chatMess)=>{
+            return(
+              <ChatMessage 
+                message = {chatMess.message}
+                sender = {chatMess.sender}
+                key = {chatMess.id}
+              />
+            )
+          })}
+        </div>
+      );
+}
+
+export default ChatMessages;
